07bb612d9040cfead8467748e80024f4ddd1704f
vVaishnavipatil <137201737+vVaishnavipatil@users.noreply.github.com>
2023-09-29 18:52:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.text', {
+    strings: ['Frontend Developer.', '&amp;  Backend Developer.'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+
+  });
+  
+  
